d96fd0515c52585f6baa1daff97abddb8dc15efa
Vasily <vasily_pavlov_98@mail.ru>
2022-03-03 17:56:19 +0700

    first commit

---
+++ Geodata.Domain/GeoEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geodata.Domain
+{
+    public class GeoEvent
+    {
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        //
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+    }
+}
